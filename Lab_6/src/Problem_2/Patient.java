0cf34387c8fc5704e50c20774402328d6c65771d
James Payne <jrpayne@email.neit.edu>
2017-05-12 08:52:22 -0400

    Lab 6 Added

--- Lab_6/src/Problem_2/Patient.java
+++ Lab_6/src/Problem_2/Patient.java
@@ -1,5 +1,25 @@
 package Problem_2;
 
 public class Patient extends Person{
+	String idNumber;
+	
+	//constructor
+	public Patient(String name) {
+		super(name);
+		idNumber = "";
+	}
 
+	//accessors and mutatord
+	public String getIdNumber() {
+		return idNumber;
+	}
+
+	public void setIdNumber(String idNumber) {
+		this.idNumber = idNumber;
+	}
+	
+	//methods
+	public void writeOutput() {
+		System.out.println("Social Security #: " + idNumber);
+	}
 }
